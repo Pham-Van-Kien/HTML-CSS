41803c2ce52f92a19659c0f52cfe212c9d42b66e
Kiên Kem <91819140+Pham-Van-Kien@users.noreply.github.com>
2022-07-08 01:07:49 +0700

    Add files via upload

---
+++ maytinh.js
@@ -0,0 +1,24 @@
+const numbers = document.querySelectorAll('div .number');
+const display = document.querySelector('.result');
+
+for(let i = 0; i < numbers.length; ++i)
+{
+    numbers[i].onclick = function(){
+        if(display.innerText === '0')
+        {
+            // nếu nội dung trong ô hiển thị nó là 0
+            // thì mình xóa nó đi
+            display.innerText = '';
+        }
+        display.innerText += numbers[i].innerText;
+    }
+}
+
+// cộng
+// lấy ra thẻ button có class add
+// mình
+// đợi thi xong
+let add = document.querySelector('.add');
+add.onclick = function(){
+    
+}
